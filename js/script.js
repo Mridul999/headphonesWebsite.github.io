4e115f0b4bdc9ee11c4da0e0a4d7c99ef9637e9c
Zihad Hosen Mridul <113275524+Mridul999@users.noreply.github.com>
2022-11-06 19:26:57 +0600

    headphoneswebsite

---
+++ js/script.js
@@ -0,0 +1,10 @@
+"use strict";
+// Navbar bg change on scroll
+let nav = document.querySelector(".navbar");
+window.onscroll = function () {
+  if (document.documentElement.scrollTop > 20) {
+    nav.classList.add("scroll-on");
+  } else {
+    nav.classList.remove("scroll-on");
+  }
+};
